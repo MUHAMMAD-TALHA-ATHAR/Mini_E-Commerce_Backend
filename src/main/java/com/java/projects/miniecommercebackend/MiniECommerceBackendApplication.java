6d8f9a543fd680fc0e22aaa0b742960ace811d85
Muhammad-Talha-Athar <muhammadtalhaathar422@gmail.com>
2026-06-01 07:58:33 +0500

    Project Setup & Configuration

---
+++ src/main/java/com/java/projects/miniecommercebackend/MiniECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.projects.miniecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniECommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniECommerceBackendApplication.class, args);
+    }
+
+}
